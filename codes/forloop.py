1cb87ace0bd9e0f8f71029d02724664c18280628
Rajat lad <01fe20bec161@kletech.ac.in>
2024-02-06 17:29:16 +0530

    first

--- codes/forloop.py
+++ codes/forloop.py
@@ -33,3 +33,15 @@ fruits = ["apple", "banana", "cherry"]
 for x in adj:
   for y in fruits:
     print(x, y)
+
+
+
+
+
+  adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
+
